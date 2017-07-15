fca13079c6fccfcae5db5dae58c20cb3598256d3
Adam Sheaffer <JJ06896@JJ0000M301211.local>
2017-07-14 19:54:02 -0700

    adding validation

--- app/user/login.component.ts
+++ app/user/login.component.ts
@@ -3,11 +3,19 @@ import { Router } from "@angular/router";
 import { AuthService } from "./auth.service";
 
 @Component({
-    templateUrl: 'app/user/login.component.html'
+    templateUrl: 'app/user/login.component.html',
+    styles: [`
+        em { 
+            float: right;
+            color: #E05C65;
+            padding-left: 10px;
+            font-size: 10px;
+        }
+    `]
 })
 export class LoginComponent {
-    constructor(private authService: AuthService, private router: Router) {}
-    
+    constructor(private authService: AuthService, private router: Router) { }
+
     login(formValues) {
         const { user, password } = formValues;
         this.authService.loginUser(user, password);
